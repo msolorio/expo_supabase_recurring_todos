f39271a68da9fc033eb53f6455b9795c9ebb3947
msolorio <msolorio.github@gmail.com>
2025-06-13 17:00:31 -0700

    [wip] setting up recurring todos

--- app/(tabs)/recurringtodos.tsx
+++ app/(tabs)/recurringtodos.tsx
@@ -1,9 +1,23 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useAuth } from "../auth/authContext";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "../lib/supabase";
 
 export default function RecurringTodos() {
+  const { session } = useAuth() as unknown as { session: Session };
+  console.log("session.user.id", session.user.id);
+
   return (
     <View style={styles.container}>
-      <Text>Recurring Todos</Text>
+      <Text style={styles.mt20}>Recurring Todos</Text>
+
+      <View>
+        <Button title="Add recurring todo" onPress={() => {}} />
+      </View>
+
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
+      </View>
     </View>
   );
 }
