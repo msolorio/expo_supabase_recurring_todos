fe5741b0c5641a6eb26c8f9e43cdf01b84e8324b
msolorio <msolorio.github@gmail.com>
2025-06-17 17:11:39 -0700

    Documentation

--- app/Auth.tsx
+++ app/Auth.tsx
@@ -4,15 +4,11 @@ import { supabase } from "./lib/supabase";
 import { Button, Input } from "@rneui/themed";
 import { Session } from "@supabase/supabase-js";
 
-// Tells Supabase Auth to continuously refresh the session automatically if
-// the app is in the foreground. When this is added, you will continue to receive
-// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
-// if the user's session is terminated. This should only be registered once.
 AppState.addEventListener("change", (state) => {
   if (state === "active") {
-    supabase.auth.startAutoRefresh();
+    supabase.auth.startAutoRefresh(); // keeps the session alive while the app is in the foreground
   } else {
-    supabase.auth.stopAutoRefresh();
+    supabase.auth.stopAutoRefresh(); // stops the session from being refreshed when the app is in the background
   }
 });
 
